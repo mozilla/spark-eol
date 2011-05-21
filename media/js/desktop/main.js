e82ae8d8b2971905718df950a75f1828dca131e4
sork <franck.git@gmail.com>
2011-05-21 22:37:45 +0200

    Added 'silent' class to prevent some elements from firing 'transitionend' events.

--- media/js/desktop/main.js
+++ media/js/desktop/main.js
@@ -64,12 +64,15 @@ $(document).ready(function() {
         firefox.changePage('layer-4');
     });
     
+    // Prevent some elements from firing 'transitionend' events
+    $('.silent').bind(TRANSITION_END, false);
+    
     // Initialize CountUp widgets
     $('.countup').each(function() {
         new CountUp($(this));
     });
     
-    // Initialize Pie chart
+    // Initialize the pie chart widget
     new PieChart('#spark-1 .block-2', 75, 75, 75, 900, [{name: 'slice1', start: -90, end: 70, color: '#ee3939'},
                                                             {name: 'slice2', start: 70, end: 200, color: '#ec5f39'},
                                                             {name: 'slice3', start: 200, end: 270, color: '#fbad46'}]);
